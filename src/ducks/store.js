41cb5a9424de368e76a2dd2a6bc54100abf357ad
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-10-17 23:15:37 -0600

    Added some very minor notes to the redux store file

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,24 +1,25 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
-import { composeWithDevTools} from 'redux-devtools-extension'
+// import { composeWithDevTools} from 'redux-devtools-extension'
 import inputsReducer from './inputsReducer'
 
 const rootReducer = combineReducers ({
     inputsReducer
 })
 
-// export default createStore(
-//     rootReducer,
-//     applyMiddleware(promiseMiddleware)
-// )
+export default createStore(
+    rootReducer,
+    applyMiddleware(promiseMiddleware)
+)
 
-const composeEnhancers = composeWithDevTools({
-    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-   });
-   const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
-    applyMiddleware(promiseMiddleware),
-    // other store enhancers if any
-   ));
+// THIS IS THE CODE YOU WOULD USE IF YOU WANTED TO ENABLE THE REDUX DEVTOOLS
+    // const composeEnhancers = composeWithDevTools({
+    //     // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+    //    });
+    //    const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    //     applyMiddleware(promiseMiddleware),
+    //     // other store enhancers if any
+    //    ));
 
-   export default store
+    //    export default store
 
